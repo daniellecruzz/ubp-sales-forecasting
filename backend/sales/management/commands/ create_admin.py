e5792b25f57c7d6db8bebaf310309e2cb7814399
daniellecruzz <danielfedii19@gmail.com>
2026-04-13 12:58:04 +0800

    Fix create_admin command

--- backend/sales/management/commands/ create_admin.py
+++ backend/sales/management/commands/ create_admin.py
@@ -2,13 +2,15 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
+    help = 'Create default admin user'
+
+    def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
                 username='admin',
-                password='admin1234',
-                email='admin@ubp.com'
+                email='admin@ubp.com',
+                password='admin1234'
             )
-            self.stdout.write('Admin user created!')
+            self.stdout.write(self.style.SUCCESS('Admin user created!'))
         else:
-            self.stdout.write('Admin already exists!')
+            self.stdout.write(self.style.SUCCESS('Admin already exists!'))
