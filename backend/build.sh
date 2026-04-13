9ba1f7766d51cfbb7c91d4664eafd6cd92ec1925
daniellecruzz <danielfedii19@gmail.com>
2026-04-13 13:01:56 +0800

    Fix admin creation using shell

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,12 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py create_admin
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@ubp.com', 'admin1234')
+    print('Admin created!')
+else:
+    print('Admin exists!')
+"
 python manage.py collectstatic --no-input
