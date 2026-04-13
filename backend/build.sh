ed9eaf01fed721cec0b59a16fed23b6f6303a661
daniellecruzz <danielfedii19@gmail.com>
2026-04-13 13:19:32 +0800

    Reset admin password

--- backend/build.sh
+++ backend/build.sh
@@ -4,10 +4,15 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
+if User.objects.filter(username='admin').exists():
+    u = User.objects.get(username='admin')
+    u.set_password('admin1234')
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('Admin password reset!')
+else:
     User.objects.create_superuser('admin', 'admin@ubp.com', 'admin1234')
     print('Admin created!')
-else:
-    print('Admin exists!')
 "
 python manage.py collectstatic --no-input
